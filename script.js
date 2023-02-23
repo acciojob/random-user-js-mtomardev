70edacdb312813571c803a849bb0641a5f0c3ae6
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-02-23 10:32:43 +0530

    try

--- script.js
+++ script.js
@@ -5,6 +5,44 @@ let additionalInfo = document.getElementById("additionalInfo");
 let getAnoutherUser = document.getElementById("getUser");
 
 
+// getAnoutherUser.addEventListener("click", function(){
+//     fetch("https://randomuser.me/api/")
+//     .then((responce) => responce.json())
+//     .then((data)=>{
+//         let user = data.results[0];
+//         image.src = user.picture.large;
+//         fullName.innerText =  user.name.title + " " + user.name.first + " " + user.name.last;
+//         fullName.dataset.age = user.dob.age;
+//         fullName.dataset.email = user.email;
+//         fullName.dataset.phone = user.phone;
+//         additionalInfo.innerHTML = "";
+//     })
+// })
+
+// let ageButton = document.querySelector('[data-attr="age"]')
+// let emailButton = document.querySelector('[data-attr="email"]')
+// let phoneButton = document.querySelector('[data-attr="phone"]')
+
+// ageButton.addEventListener("click", function(){
+//     additionalInfo.innerHTML = fullName.dataset.age;
+// })
+
+// emailButton.addEventListener("click", function(){
+//     additionalInfo.innerHTML = fullName.dataset.email; 
+// })
+
+// phoneButton.addEventListener("click", function(){
+//     additionalInfo.innerHTML = fullName.dataset.phone; 
+// })
+
+// getAnoutherUser.click();
+
+
+
+let age;
+let email;
+let phone;
+
 getAnoutherUser.addEventListener("click", function(){
     fetch("https://randomuser.me/api/")
     .then((responce) => responce.json())
@@ -12,9 +50,9 @@ getAnoutherUser.addEventListener("click", function(){
         let user = data.results[0];
         image.src = user.picture.large;
         fullName.innerText =  user.name.title + " " + user.name.first + " " + user.name.last;
-        fullName.dataset.age = user.dob.age;
-        fullName.dataset.email = user.email;
-        fullName.dataset.phone = user.phone;
+        age = user.dob.age;
+        email = user.email;
+        phone = user.phone;
         additionalInfo.innerHTML = "";
     })
 })
@@ -24,15 +62,15 @@ let emailButton = document.querySelector('[data-attr="email"]')
 let phoneButton = document.querySelector('[data-attr="phone"]')
 
 ageButton.addEventListener("click", function(){
-    additionalInfo.innerHTML = fullName.dataset.age;
+    additionalInfo.innerHTML = age;
 })
 
 emailButton.addEventListener("click", function(){
-    additionalInfo.innerHTML = fullName.dataset.email; 
+    additionalInfo.innerHTML = email; 
 })
 
 phoneButton.addEventListener("click", function(){
-    additionalInfo.innerHTML = fullName.dataset.phone; 
+    additionalInfo.innerHTML = phone; 
 })
 
 getAnoutherUser.click();
